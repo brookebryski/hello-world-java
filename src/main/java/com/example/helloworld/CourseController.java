7f941021082f04245c005a2f43094ba007f462ea
BrookeBryski <brookeabryski@gmail.com>
2021-11-26 16:40:44 -0600

    second route

--- src/main/java/com/example/helloworld/CourseController.java
+++ src/main/java/com/example/helloworld/CourseController.java
@@ -11,6 +11,15 @@ public class CourseController {
     //http://localhost8080/courses
     @GetMapping("/courses")
     public List<Course> getAllCourses() {
-        return Arrays.asList(new Course(1,"Learn Microservices", "Brooke"));
+        return Arrays.asList(new Course(1,"Learn Microservices", "Brooke"),
+                new Course(2,"Learn Full Stack with Angular and React", "Brooke"));
+    }
+
+    //http://localhost8080/courses/1
+
+    @GetMapping("/courses/1")
+    public Course getCourseDetails() {
+        return new Course(1,"Learn Microservices", "Brooke");
+
     }
 }
